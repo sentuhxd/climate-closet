093742664d81a58e49f99a191f3da8e52829a187
ross402 <65184393+ross402@users.noreply.github.com>
2023-09-06 00:14:29 -0400

    Get the air quality statistics based on the current lat and long

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ window.navigator.geolocation.getCurrentPosition(function (data) {
   currentLongitude = data.coords.longitude;
   console.log(currentLatitude + " " + currentLongitude);
   getWeatherData();
+  getAirQuality();
 });
 
 //Function to use current location to get weather
@@ -22,6 +23,27 @@ var getWeatherData = function () {
     "2%2C" +
     currentLongitude +
     "?unitGroup=metric&key=NAGXW443LV2TCEF9VGRU22RM9&contentType=json";
+
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log(data);
+      });
+    } else {
+      alert("Error: " + response.statusText);
+    }
+  });
+};
+
+//Function to retrieve the air quality in the current area
+
+var getAirQuality = function () {
+  var apiUrl =
+    "https://api.waqi.info/feed/geo:" +
+    currentLatitude +
+    ";" +
+    currentLongitude +
+    "?token=835ba69f51164e27a1bc49f8ff790b8645c8840a";
 
   fetch(apiUrl).then(function (response) {
     if (response.ok) {
