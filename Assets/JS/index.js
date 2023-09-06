8190939dfa62a08061ee53366a3c141f7e94caea
ross402 <65184393+ross402@users.noreply.github.com>
2023-09-06 13:47:38 -0400

    Added functionality for assigning the current user in local storage to be retrieved in the user profile also error checking for entering in user name

---
+++ Assets/JS/index.js
@@ -0,0 +1,50 @@
+//DOM
+var userProfileButton = document.querySelector("#user-profile-button");
+var userInput = document.querySelector("#first_name2");
+
+//DATA
+
+//FUNCTIONS
+
+//Create a function to get the local storage list and check if the user name is contained in that list
+function checkName() {
+  var nameList = JSON.parse(localStorage.getItem("user-list"));
+  var nameBoolean = false;
+
+  if (nameList === null) {
+    console.log("ERROR NO CURRERNT USERS");
+    return nameBoolean;
+  }
+
+  for (var i = 0; i < nameList.length; i++) {
+    var currentPerson = nameList[i].name;
+
+    if (currentPerson === userInput.value) {
+      nameBoolean = true;
+    }
+  }
+  //if the user is never in the list of current users then return false and error
+  //If the user is true then we will update who the current user is
+  if (!nameBoolean) {
+    console.log("ERROR USER NOT IN LIST");
+    return nameBoolean;
+  } else {
+    localStorage.setItem("current-user", userInput.value);
+    return nameBoolean;
+  }
+}
+
+//USER INTERACTIONS
+//This will track when the user clicks enter on the homepage
+userProfileButton.addEventListener("click", function () {
+  //Call check name function
+  var checkBoolean = checkName();
+
+  if (checkBoolean) {
+    window.location = "userprofile.html";
+  } else {
+    console.log("ERROR ENTER IN A PROPER NAME");
+  }
+});
+
+//INITIALIZATIONS
